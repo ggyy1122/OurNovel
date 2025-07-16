fdcdfd51d4f9921e0ee6f520f586347f61573513
sherrylxf <1298512407@qq.com>
2025-07-16 11:12:02 +0800

    更新Rate不可更改评分

--- BackEnd/Repositories/IRateRepository.cs
+++ BackEnd/Repositories/IRateRepository.cs
@@ -1,11 +1,11 @@
 ﻿using OurNovel.Models;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public interface IRateRepository
 {
-    Task AddOrUpdateAsync(Rate rate);
+    Task AddAsync(Rate entity);
     Task DeleteAsync(int novelId, int readerId);
+    Task<Rate?> FindAsync(int novelId, int readerId);  // 新增
     Task<IEnumerable<Rate>> GetByNovelIdAsync(int novelId);
     Task<IEnumerable<Rate>> GetByReaderIdAsync(int readerId);
     Task<IEnumerable<Rate>> GetAllAsync();
