29e3837cffd1c0545780859ff6c12f78ebb9b851
sherrylxf <1298512407@qq.com>
2025-07-22 16:50:21 +0800

    ranking的3个api增加小说所有信息

--- BackEnd/Models/RecommendRankingDto.cs
+++ BackEnd/Models/RecommendRankingDto.cs
@@ -7,9 +7,22 @@ namespace OurNovel.Models.Dto
     {
         public int NovelId { get; set; }
         public string NovelName { get; set; } = null!;
+        public int AuthorId { get; set; }
         public string AuthorName { get; set; } = null!;
+
         public string CoverUrl { get; set; } = null!;
         public string Introduction { get; set; } = null!;
-        public int RecommendedCount { get; set; }  // 推荐数量
+
+        public string? Status { get; set; }
+        public DateTime? CreateTime { get; set; }
+
+        public int? Score { get; set; }
+        public int CollectedCount { get; set; }  
+
+        public long? TotalWordCount { get; set; }
+        public decimal? TotalPrice { get; set; }
+
+
+        public int RecommendCount { get; set; }  // 推荐数量
     }
 }
